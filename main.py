af6430effe844d7b5adc1e078b2d145cc8ba3290
Kartik Kumar <Kartik.Kumar@Comvision.io>
2024-02-01 22:12:05 +0530

    modifications in main app

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI, WebSocket
+from fastapi.encoders import jsonable_encoder
 from routers.user_management import user_manager
-
 from config.constants import AUTH_TOKEN, TWILIO_SID
+from config.database_config import engine
+from models.models import Base
+import json
 
-app = FastAPI()
+app = FastAPI(title= "Socially")
 app.include_router(user_manager)
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    acceptence= await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        print(data)
         await websocket.send_text(f"You sent: {data}")
+
+
+# @app.on_event('startup')
+# async def startup():
+#     Base.metadata.create_all(bind= engine)
