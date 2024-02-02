705c3295b5b556d809d747a2ad90648d522ee764
Kartik Kumar <Kartik.Kumar@Comvision.io>
2024-02-02 05:31:23 +0530

    id generation,uniqueness anf len constraints on user model

--- models/models.py
+++ models/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, VARCHAR, TIMESTAMP, INTEGER
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+from uuid import uuid4
 
 Base = declarative_base()
 
@@ -9,11 +10,11 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(UUID(as_uuid=True), primary_key=True)
-    username = Column(VARCHAR)
-    name = Column(VARCHAR)
-    mobile = Column(VARCHAR)
-    email = Column(VARCHAR)
+    id = Column(UUID(as_uuid=True), primary_key=True, default= uuid4)
+    username = Column(VARCHAR(25), unique= True)
+    name = Column(VARCHAR(50))
+    mobile = Column(VARCHAR, unique= True)
+    email = Column(VARCHAR, unique= True)
     about = Column(VARCHAR)
     joined = Column(TIMESTAMP, default= datetime.now())
 
