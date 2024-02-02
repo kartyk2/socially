c995d1cbb2e2b25b0bdd8be1db09c4fff05cc7e4
Kartik Kumar <Kartik.Kumar@Comvision.io>
2024-02-02 05:30:04 +0530

    added table re-creation functionality

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy import text
 from routers.user_management import user_manager
 from config.constants import AUTH_TOKEN, TWILIO_SID
-from config.database_config import engine
+from config.database_config import engine, DB_URI, DatabaseSessionsManager
 from models.models import Base
 import json
 
@@ -20,4 +21,17 @@ async def websocket_endpoint(websocket: WebSocket):
 
 # @app.on_event('startup')
 # async def startup():
+#     #drop all tables
+#     with DatabaseSessionsManager(DB_URI) as db:
+#         db.execute(text(
+#             """
+#             DROP SCHEMA public CASCADE;
+#             CREATE SCHEMA public;
+
+#             GRANT ALL ON SCHEMA public TO postgres;
+#             """
+#         ))
+#         db.commit()
+        
+#     #create all user defined tables
 #     Base.metadata.create_all(bind= engine)
