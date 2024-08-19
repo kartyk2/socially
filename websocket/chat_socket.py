82d73402cbe4528949d4da6be90c9cb9010fffc2
Kartik Kumar <Kartik.Kumar@Comvision.io>
2024-08-19 14:29:49 +0530

    default topic for producing messages

--- websocket/chat_socket.py
+++ websocket/chat_socket.py
@@ -1,23 +1,23 @@
 from kafka_config.producer import kafka_producer
+from config.constants import get_settings
 from datetime import datetime
 import socketio
 import uuid
 
-sio = socketio.AsyncServer(cors_allowed_origins="*")
+sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode='asgi')
 sio_app = socketio.ASGIApp(sio)
 
-# Dictionary to keep track of connected clients
+setting= get_settings()
 connected_clients = {}
 
 @sio.event
 async def connect(sid, environ):
-    user_id = str(uuid.uuid4())  # Example user ID
+    user_id = str(uuid.uuid4())
     connected_clients[user_id] = sid
     print(f"User connected: {user_id}")
 
 @sio.event
 async def disconnect(sid):
-    # Find and remove the user_id from connected_clients
     for user_id, socket_id in connected_clients.items():
         if socket_id == sid:
             del connected_clients[user_id]
@@ -26,12 +26,11 @@ async def disconnect(sid):
 
 @sio.event
 async def message(sid, data):
-    # Process incoming messages and send to Kafka
     message_payload = {
         "user_id": next((uid for uid, socket_id in connected_clients.items() if socket_id == sid), None),
         "message": data,
         "timestamp": datetime.now().isoformat()
     }
 
-    kafka_producer.send('your_topic', value=message_payload)
+    kafka_producer.send(setting.kafka_topic, value=message_payload)
     print(f"Message sent to Kafka: {message_payload}")
